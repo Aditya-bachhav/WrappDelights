cca2c831fec4f58d5fc641fe0b21d65b541c1f6a
Aditya Bachhav <aditya.bachhav.00@gmail.com>
2026-03-24 20:43:33 +0530

    Fixed app import path for deployment

--- delights_backend/core/core/urls.py
+++ delights_backend/core/core/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-
+from delights_backend.core.store import models
 from django.contrib.auth import views as auth_views
 from store.views import (
     corporate_page,
