87c27c95d3bbcc07b1e72bc74e5ec2f6cc15e0cf
Aditya Bachhav <aditya.bachhav.00@gmail.com>
2026-04-07 00:58:54 +0530

    Fix Render server config: use ensure_superuser, create media dir

--- build.sh
+++ build.sh
@@ -7,11 +7,5 @@ pip install -r requirements.txt
 echo "Entering Django project directory..."
 cd delights_backend/core
 
-echo "Running migrations..."
-python manage.py migrate --noinput
-
-echo "Ensuring superuser from environment variables..."
-python manage.py ensure_superuser
-
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
